c0b3eba6b4ae8d43bb98c6d0ab91139c77d09ba7
isther <28251536@qq.com>
2022-01-15 21:47:34 +0800

    Test file

--- pkg/cgroup/build_test.go
+++ pkg/cgroup/build_test.go
@@ -1,11 +1,52 @@
 package cgroup
 
 import (
-	"log"
 	"testing"
+	"time"
 )
 
 func TestNewBuilder(t *testing.T) {
-	cgroupBuilder := NewBuilder().AddCPU().AddCPUAcct().AddCPUSet().AddCPU().AddMemory().AddPids()
-	log.Println(cgroupBuilder)
+	cgroupBuilder := NewBuilder().AddType("V2").AddCPU().AddCPUAcct().AddCPUSet().AddCPU().AddMemory().AddPids()
+
+	cg, err := cgroupBuilder.Build("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		cg.Destroy()
+	})
+
+	if err := cg.SetCPUQuota(50000, 100000); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := cg.SetCPUSet("6"); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := cg.SetMemoryLimit(256); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := cg.SetProcLimit(10); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := cg.AddProc(uint64(138898)); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := cg.CPUUsage(); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := cg.MemoryUsage(); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := cg.MemoryMaxUsage(); err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(5000 * time.Millisecond)
 }
