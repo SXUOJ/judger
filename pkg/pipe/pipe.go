fd85d6150b8260c826e3552b102435b2eff10086
isther <28251536@qq.com>
2022-01-22 20:35:34 +0800

    initialization pipe package

---
+++ pkg/pipe/pipe.go
@@ -0,0 +1,35 @@
+package pipe
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+func NewPipe() (*os.File, *os.File, error) {
+	read, write, err := os.Pipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	return read, write, nil
+}
+
+func SendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	logrus.Infof("command all is %s", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
+}
+
+func ReadUserCommand() []string {
+	pipe := os.NewFile(uintptr(3), "pipe")
+	msg, err := ioutil.ReadAll(pipe)
+	if err != nil {
+		logrus.Errorf("init read pipe error %v", err)
+		return nil
+	}
+	msgStr := string(msg)
+	return strings.Split(msgStr, " ")
+}
