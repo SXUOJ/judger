49fca9016e0e93a5e60d7a7b3af1431d320a7baa
isther <28251536@qq.com>
2022-02-13 22:53:18 +0800

    Add namespace pkg

---
+++ pkg/namespace/namespace.go
@@ -0,0 +1,64 @@
+package namespace
+
+import (
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+)
+
+func Init(newRoot string) error {
+	logrus.Info("Init namespace start")
+
+	if err := pivotRoot(newRoot); err != nil {
+		return err
+	}
+
+	if err := syscall.Sethostname([]byte("judge")); err != nil {
+		return err
+	}
+
+	defaultMountFlag := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "prco", uintptr(defaultMountFlag), "")
+
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mod=755")
+
+	pd, _ := os.Getwd()
+	logrus.Info("Init namespace end", pd)
+	return nil
+}
+
+func pivotRoot(newRoot string) error {
+	if err := syscall.Mount(newRoot, newRoot, "", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		return err
+	}
+
+	// create put_old directory
+	rootOld := filepath.Join(newRoot, "/.pivot_root")
+	if err := os.MkdirAll(rootOld, 0700); err != nil {
+		return err
+	}
+
+	// call pivotRoot
+	if err := syscall.PivotRoot(newRoot, rootOld); err != nil {
+		return err
+	}
+
+	if err := os.Chdir("/"); err != nil {
+		return err
+	}
+
+	// umount put_old, which now lives at /.pivot_root
+	rootOld = filepath.Join("/", ".pivot_root")
+	if err := syscall.Unmount(rootOld, syscall.MNT_DETACH); err != nil {
+		return err
+	}
+
+	// remove put_old
+	if err := os.RemoveAll(rootOld); err != nil {
+		return err
+	}
+
+	return nil
+}
