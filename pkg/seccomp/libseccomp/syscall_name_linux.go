e1d8d233d27224cd0351d95c7d26ecfda8abdcdf
isther <28251536@qq.com>
2022-04-05 20:16:36 +0800

    Added function -- convert syscall

---
+++ pkg/seccomp/libseccomp/syscall_name_linux.go
@@ -0,0 +1,21 @@
+package libseccomp
+
+import (
+	"fmt"
+
+	"github.com/elastic/go-seccomp-bpf/arch"
+)
+
+var info, errInfo = arch.GetInfo("")
+
+// ToSyscallName convert syscallnum to syscall name
+func ToSyscallName(sysnum uint) (string, error) {
+	if errInfo != nil {
+		return "", errInfo
+	}
+	n, ok := info.SyscallNumbers[int(sysnum)]
+	if !ok {
+		return "", fmt.Errorf("syscall no %d does not exits", sysnum)
+	}
+	return n, nil
+}
