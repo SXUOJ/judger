d81000ca3d1b20739b09207ee28b33328128ef45
isther <28251536@qq.com>
2022-04-10 21:08:24 +0800

    Modifed object "forkexec.Runner"

--- pkg/forkexec/runner.go
+++ pkg/forkexec/runner.go
@@ -1,3 +1,21 @@
 package forkexec
 
-type Runner struct{}
+import (
+	"syscall"
+
+	"github.com/Sxu-Online-Judge/judge/pkg/rlimit"
+)
+
+type Runner struct {
+	Args     []string
+	Env      []string
+	ExecFile uintptr
+	RLimits  []rlimit.RLimit
+	Files    []uintptr
+	WorkDir  string
+	Ptrace   bool
+	Seccomp  *syscall.SockFprog
+
+	CloneFlags uintptr
+	SyncFunc   func(int) error
+}
