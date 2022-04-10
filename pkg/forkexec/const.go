414d1d33fcc1c19df7eafeef7b1f6179acf29de4
isther <28251536@qq.com>
2022-04-10 21:07:42 +0800

    Added define

--- pkg/forkexec/const.go
+++ pkg/forkexec/const.go
@@ -1,1 +1,22 @@
 package forkexec
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+const (
+	SECCOMP_SET_MODE_STRICT   = 0
+	SECCOMP_SET_MODE_FILTER   = 1
+	SECCOMP_FILTER_FLAG_TSYNC = 1
+
+	UnshareFlags = unix.CLONE_NEWIPC | unix.CLONE_NEWNET | unix.CLONE_NEWNS |
+		unix.CLONE_NEWPID | unix.CLONE_NEWUSER | unix.CLONE_NEWUTS | unix.CLONE_NEWCGROUP
+)
+
+var (
+	empty = []byte("\000")
+
+	etxtbsyRetryInterval = unix.Timespec{
+		Nsec: 1 * 1000 * 1000,
+	}
+)
