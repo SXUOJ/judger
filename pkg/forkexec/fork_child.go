4270d62cb9c248c49a3122f3f14565bd6f57abea
isther <28251536@qq.com>
2022-04-10 21:41:47 +0800

    Modifed forkAndExecInChild()

--- pkg/forkexec/fork_child.go
+++ pkg/forkexec/fork_child.go
@@ -1,8 +1,41 @@
 package forkexec
 
-import "syscall"
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
 
 func forkAndExecInChild(r *Runner, argv0 *byte, argv, env []*byte, workdir *byte, p [2]int) (r1 uintptr, err1 syscall.Errno) {
+	var (
+		idx        int
+		pipe       int
+		forkErr    ForkError
+		childError ChildError
+	)
+	fd, nextfd := prepareFds(r.Files)
 
+	syscall.ForkLock.Lock()
+	beforeFork()
+	r1, _, err1 = syscall.RawSyscall6(syscall.SYS_CLONE, uintptr(syscall.SIGCHLD)|(r.CloneFlags&UnshareFlags), 0, 0, 0, 0, 0)
+	if err1 != 0 || r1 != 0 {
+		fmt.Println("in parent process")
+		return
+	}
+	afterForkInChild()
+
+	pipe, forkErr, idx, err1 = forkAndExecInChild1(r, argv0, argv, env, workdir, fd, nextfd, p)
+
+	childError.Err = err1
+	childError.ForkErr = forkErr
+	childError.Index = idx
+
+	// send error code on pipe
+	syscall.RawSyscall(unix.SYS_WRITE, uintptr(pipe), uintptr(unsafe.Pointer(&childError)), unsafe.Sizeof(childError))
+	for {
+		syscall.RawSyscall(syscall.SYS_EXIT, uintptr(err1), 0, 0)
+	}
 	return
 }
