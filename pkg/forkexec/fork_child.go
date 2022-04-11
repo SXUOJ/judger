571caee42a4e97b63e45e4102b1844df412fe5bf
isther <28251536@qq.com>
2022-04-11 09:25:51 +0800

    Added function link

--- pkg/forkexec/fork_child.go
+++ pkg/forkexec/fork_child.go
@@ -1,35 +1,35 @@
 package forkexec
 
 import (
-	"fmt"
 	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
 
+// Reference to src/syscall/exec_linux.go
+//go:norace
 func forkAndExecInChild(r *Runner, argv0 *byte, argv, env []*byte, workdir *byte, p [2]int) (r1 uintptr, err1 syscall.Errno) {
 	var (
-		idx        int
-		pipe       int
-		forkErr    ForkError
+		loc        ForkError
+		idx, pipe  int
 		childError ChildError
 	)
 	fd, nextfd := prepareFds(r.Files)
 
 	syscall.ForkLock.Lock()
 	beforeFork()
-	r1, _, err1 = syscall.RawSyscall6(syscall.SYS_CLONE, uintptr(syscall.SIGCHLD)|(r.CloneFlags&UnshareFlags), 0, 0, 0, 0, 0)
+	r1, _, err1 = syscall.RawSyscall6(syscall.SYS_CLONE, uintptr(syscall.SIGCHLD), 0, 0, 0, 0, 0)
 	if err1 != 0 || r1 != 0 {
-		fmt.Println("in parent process")
 		return
 	}
 	afterForkInChild()
 
-	pipe, forkErr, idx, err1 = forkAndExecInChild1(r, argv0, argv, env, workdir, fd, nextfd, p)
+	pipe, loc, idx, err1 = forkAndExecInChild1(r, argv0, argv, env, workdir, fd, nextfd, p)
 
+	// send error code on pipe
 	childError.Err = err1
-	childError.ForkErr = forkErr
+	childError.ForkErr = loc
 	childError.Index = idx
 
 	// send error code on pipe
@@ -39,21 +39,21 @@ func forkAndExecInChild(r *Runner, argv0 *byte, argv, env []*byte, workdir *byte
 	}
 }
 
-func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byte, fd []int, nextfd int, p [2]int) (pipe int, forkError ForkError, idx int, err1 syscall.Errno) {
-	var (
-		r1      uintptr
-		pid     uintptr
-		err2    syscall.Errno
-		unshare = r.CloneFlags&unix.CLONE_NEWUSER == unix.CLONE_NEWUSER
-	)
+//go:nosplit
+func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byte, fd []int, nextfd int, p [2]int) (pipe int, loc ForkError, idx int, err1 syscall.Errno) {
 	pipe = p[1]
+	var (
+		r1, pid     uintptr
+		err2        syscall.Errno
+		unshareUser = r.CloneFlags&unix.CLONE_NEWUSER == unix.CLONE_NEWUSER
+	)
 
 	// Close write end of pipe
 	if _, _, err1 = syscall.RawSyscall(syscall.SYS_CLOSE, uintptr(p[0]), 0, 0); err1 != 0 {
 		return pipe, FErrorCloseWrite, 0, err1
 	}
 
-	if unshare {
+	if unshareUser {
 		r1, _, err1 = syscall.RawSyscall(syscall.SYS_READ, uintptr(pipe), uintptr(unsafe.Pointer(&err2)), unsafe.Sizeof(err2))
 		if err1 != 0 {
 			return pipe, FErrorUnshareUserRead, 0, err1
@@ -74,7 +74,6 @@ func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byt
 		return pipe, FErrorGetPid, 0, err1
 	}
 
-	// assigns fds for child process
 	if pipe < nextfd {
 		_, _, err1 = syscall.RawSyscall(syscall.SYS_DUP3, uintptr(pipe), uintptr(nextfd), syscall.O_CLOEXEC)
 		if err1 != 0 {
@@ -125,13 +124,13 @@ func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byt
 		}
 	}
 
-	// Set the session ID
+	// set session id
 	_, _, err1 = syscall.RawSyscall(syscall.SYS_SETSID, 0, 0, 0)
 	if err1 != 0 {
 		return pipe, FErrorSetSid, 0, err1
 	}
 
-	// chdir for child
+	//  chdir
 	if workdir != nil {
 		_, _, err1 = syscall.RawSyscall(syscall.SYS_CHDIR, uintptr(unsafe.Pointer(workdir)), 0, 0)
 		if err1 != 0 {
@@ -139,9 +138,8 @@ func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byt
 		}
 	}
 
-	// Set limit
+	// set rlimit
 	for i, rlim := range r.RLimits {
-		// prlimit instead of setrlimit to avoid 32-bit limitation (linux > 3.2)
 		_, _, err1 = syscall.RawSyscall6(syscall.SYS_PRLIMIT64, 0, uintptr(rlim.Res), uintptr(unsafe.Pointer(&rlim.Rlim)), 0, 0, 0)
 		if err1 != 0 {
 			return pipe, FErrorSetRlimit, i, err1
@@ -156,7 +154,6 @@ func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byt
 		}
 	}
 
-	// Enable Ptrace & sync with parent (since ptrace_me is a blocking operation)
 	if r.Ptrace && r.Seccomp != nil {
 		{
 			r1, _, err1 = syscall.RawSyscall(syscall.SYS_WRITE, uintptr(pipe), uintptr(unsafe.Pointer(&err2)), uintptr(unsafe.Sizeof(err2)))
@@ -168,7 +165,6 @@ func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byt
 			if r1 == 0 || err1 != 0 {
 				return pipe, FErrorSyncRead, 0, err1
 			}
-
 		}
 		_, _, err1 = syscall.RawSyscall(syscall.SYS_PTRACE, uintptr(syscall.PTRACE_TRACEME), 0, 0)
 		if err1 != 0 {
@@ -176,7 +172,6 @@ func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byt
 		}
 	}
 
-	// traced execve, thus child need parent attached to it first
 	if r.Seccomp != nil && r.Ptrace {
 		// Stop to wait for ptrace tracer
 		_, _, err1 = syscall.RawSyscall(syscall.SYS_KILL, pid, uintptr(syscall.SIGSTOP), 0)
@@ -185,16 +180,14 @@ func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byt
 		}
 	}
 
-	// Load seccomp, stop and wait for tracer
+	// load seccomp filter
 	if r.Seccomp != nil && r.Ptrace {
-		// Load seccomp filter
 		_, _, err1 = syscall.RawSyscall(unix.SYS_SECCOMP, SECCOMP_SET_MODE_FILTER, SECCOMP_FILTER_FLAG_TSYNC, uintptr(unsafe.Pointer(r.Seccomp)))
 		if err1 != 0 {
 			return pipe, FErrorSeccomp, 0, err1
 		}
 	}
 
-	// Before exec, sync with parent through pipe (configured as close_on_exec)
 	if !r.Ptrace || r.Seccomp == nil {
 		{
 			r1, _, err1 = syscall.RawSyscall(syscall.SYS_WRITE, uintptr(pipe), uintptr(unsafe.Pointer(&err2)), uintptr(unsafe.Sizeof(err2)))
@@ -230,7 +223,6 @@ func forkAndExecInChild1(r *Runner, argv0 *byte, argv, env []*byte, workdir *byt
 		if err1 != syscall.ETXTBSY {
 			break
 		}
-		// wait instead of busy wait
 		syscall.RawSyscall(unix.SYS_NANOSLEEP, uintptr(unsafe.Pointer(&etxtbsyRetryInterval)), 0, 0)
 		if r.ExecFile > 0 {
 			_, _, err1 = syscall.RawSyscall6(unix.SYS_EXECVEAT, r.ExecFile,
