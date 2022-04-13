8d838f722a1457d00560697602a81796d898eb94
isther <28251536@qq.com>
2022-04-13 17:34:57 +0800

    Added config

---
+++ config/config.go
@@ -0,0 +1,97 @@
+package config
+
+// This file includes configs for the run program settings
+
+var (
+	// default allowed safe syscalls
+	defaultSyscallAllows = []string{
+		// file access through fd
+		"read",
+		"write",
+		"readv",
+		"writev",
+		"close",
+		"fstat",
+		"lseek",
+		"dup",
+		"dup2",
+		"dup3",
+		"ioctl",
+		"fcntl",
+		"fadvise64",
+		"pread64",
+		"pwrite64",
+
+		// memory action
+		"mmap",
+		"mprotect",
+		"munmap",
+		"brk",
+		"mremap",
+		"msync",
+		"mincore",
+		"madvise",
+
+		// signal action
+		"rt_sigaction",
+		"rt_sigprocmask",
+		"rt_sigreturn",
+		"rt_sigpending",
+		"sigaltstack",
+
+		// get current work dir
+		"getcwd",
+
+		// process exit
+		"exit",
+		"exit_group",
+
+		// others
+		"arch_prctl",
+
+		"gettimeofday",
+		"getrlimit",
+		"getrusage",
+		"times",
+		"time",
+		"clock_gettime",
+
+		"restart_syscall",
+	}
+
+	// default syscalls to trace
+	defaultSyscallTraces = []string{
+		// execute file
+		"execve",
+		"execveat",
+
+		// file open
+		"open",
+		"openat",
+
+		// file delete
+		"unlink",
+		"unlinkat",
+
+		// soft link
+		"readlink",
+		"readlinkat",
+
+		// permission check
+		"lstat",
+		"stat",
+		"access",
+		"faccessat",
+	}
+
+	// process related syscall if allowProc enabled
+	defaultProcSyscalls = []string{"clone", "fork", "vfork", "nanosleep", "execve"}
+
+	// config for different type of program
+	// workpath and arg0 have additional read / stat permission
+	runptraceConfig = map[string]ProgramConfig{
+		"default": {
+			Syscall: SyscallConfig{},
+		},
+	}
+)
