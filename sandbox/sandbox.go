4d96f2af68bb93a846d3de875881844d20ca6985
isther <28251536@qq.com>
2022-04-13 17:34:32 +0800

    Run

--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -1,20 +1,61 @@
 package sandbox
 
 import (
+	"context"
+
+	"github.com/Sxu-Online-Judge/judge/pkg/forkexec"
+	"github.com/Sxu-Online-Judge/judge/pkg/rlimit"
+	"github.com/Sxu-Online-Judge/judge/pkg/seccomp"
+	"github.com/Sxu-Online-Judge/judge/ptrace"
 	"github.com/Sxu-Online-Judge/judge/runner"
 )
 
-type Sandbox struct {
-	Handler
-	Runner
-	runner.Limit
+type Runner struct {
+	Args     []string
+	Env      []string
+	WorkDir  string
+	ExecFile uintptr
+	Files    []uintptr
+	RLimits  []rlimit.RLimit
+	Seccomp  seccomp.Filter
+	SyncFunc func(pid int) error
+
+	Handler             Handler
+	ShowDetails, Unsafe bool
+
+	Limit runner.Limit
 }
 
 type Handler interface {
-	Handle(*Context) TraceAction
-	Debug(v ...interface{})
+	// CheckRead(string) ptrace.TraceAction
+	// CheckWrite(string) ptrace.TraceAction
+	// CheckStat(string) ptrace.TraceAction
+	CheckSyscall(string) ptrace.TraceAction
 }
 
-type Runner interface {
-	Start() (int, error)
+func (r *Runner) Run(c context.Context) runner.Result {
+	fork := &forkexec.Runner{
+		Args:     r.Args,
+		Env:      r.Env,
+		ExecFile: r.ExecFile,
+		RLimits:  r.RLimits,
+		Files:    r.Files,
+		WorkDir:  r.WorkDir,
+		Seccomp:  r.Seccomp.SockFprog(),
+		Ptrace:   true,
+		SyncFunc: r.SyncFunc,
+	}
+
+	th := &tracerHandler{
+		ShowDetails: r.ShowDetails,
+		Unsafe:      r.Unsafe,
+		Handler:     r.Handler,
+	}
+
+	tracer := ptrace.Ptracer{
+		Handler: th,
+		Runner:  fork,
+		Limit:   r.Limit,
+	}
+	return tracer.Trace(c)
 }
