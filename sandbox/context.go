eacde6fd9b4722d229a9bafcfcbdbad37189c8cf
isther <28251536@qq.com>
2022-04-13 17:35:20 +0800

    Removed useless code

--- sandbox/context.go
+++
@@ -1,8 +0,0 @@
-package sandbox
-
-import "syscall"
-
-type Context struct {
-	Pid  int
-	regs syscall.PtraceRegs
-}
--- sandbox/trace.go
+++
@@ -1,9 +0,0 @@
-package sandbox
-
-type TraceAction int
-
-const (
-	ActionAllow TraceAction = iota
-	ActionTrace
-	ActionKill
-)
