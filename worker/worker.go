9fe7ebb2eb02f4ce9af3f34e0852708a9f95a912
isther <28251536@qq.com>
2022-04-15 22:16:04 +0800

    compile after to run

--- worker/worker.go
+++ worker/worker.go
@@ -3,10 +3,14 @@ package worker
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/SXUOJ/judge/config"
@@ -15,13 +19,16 @@ import (
 	"github.com/SXUOJ/judge/pkg/seccomp"
 	"github.com/SXUOJ/judge/runner"
 	"github.com/SXUOJ/judge/sandbox"
+	"github.com/SXUOJ/judge/util"
+	"github.com/sirupsen/logrus"
 )
 
 var (
-	showDetails = true
-	unSafe      = true
-	runDir      = filepath.Join("./tmp")
-	pathEnv     = "PATH=/usr/local/bin:/usr/bin:/bin"
+	showDetails   = false
+	unSafe        = false
+	runDir        = filepath.Join("./tmp")
+	sampleBaseDir = filepath.Join(".", "tmp", "sample")
+	pathEnv       = "PATH=/usr/local/bin:/usr/bin:/bin"
 )
 
 type Worker struct {
@@ -46,6 +53,8 @@ func (worker *Worker) Run() (Results, error) {
 		result     *Result
 		sourcePath = filepath.Join(worker.WorkDir, worker.FileName)
 		binaryPath = filepath.Join(worker.WorkDir, worker.SubmitID)
+		sampleDir  = filepath.Join(sampleBaseDir, worker.ProblemID)
+		outputDir  = filepath.Join(worker.WorkDir)
 	)
 
 	lang, err := lang.NewLang(worker.Type, sourcePath, binaryPath)
@@ -74,6 +83,47 @@ func (worker *Worker) Run() (Results, error) {
 			}, nil
 		}
 	}
+
+	sampleCount := 0
+	files, _ := ioutil.ReadDir(sampleDir)
+	for _, fi := range files {
+		if fi.IsDir() {
+			continue
+		} else {
+			sampleCount++
+		}
+	}
+
+	if ok, err := util.PathExists(outputDir); err != nil {
+		logrus.Println("Check if path exists failed")
+	} else {
+		if ok {
+			logrus.Println("Output dir exists: ", outputDir)
+		} else {
+			if err := os.MkdirAll(outputDir, 0755); err != nil {
+				logrus.Println("Make dir failed")
+				return nil, nil
+			}
+		}
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < sampleCount/2; i++ {
+		wg.Add(1)
+		go func(id int) {
+			r, err := worker.load(id, lang)
+			if err != nil {
+				logrus.Error("worker.load() failed")
+			}
+			res, err := runByOne(r, lang.CompileRealTimeLimit())
+			if res.Status != runner.StatusNormal {
+				logrus.Error("runByOne failed")
+			}
+			defer wg.Done()
+		}(i + 1)
+	}
+	wg.Wait()
+
 	return result, nil
 }
 
@@ -104,17 +154,11 @@ func (worker *Worker) load(sampleID int, lang lang.Lang) (*sandbox.Runner, error
 		}, nil
 	}
 
-	var (
-		inputFileName  = ""
-		outputFileName = ""
-		errorFileName  = ""
-	)
-
 	defaultAction, allow, trace, h := config.GetConf(strings.Join([]string{worker.Type, "run"}, ""), worker.AllowProc)
 
 	// limit
 	rlimits, limit := parseLimit(worker.TimeLimit, worker.RealTimeLimit, worker.OutputLimit, worker.StackLimit, worker.MemoryLimit)
-
+	log.Println("defaultAction: ", defaultAction)
 	// build seccomp filter
 	seccompBuilder := seccomp.Builder{
 		Default: defaultAction,
@@ -124,6 +168,19 @@ func (worker *Worker) load(sampleID int, lang lang.Lang) (*sandbox.Runner, error
 	filter, _ := seccompBuilder.Build()
 
 	// load fds
+	var (
+		sampleDir = filepath.Join(sampleBaseDir, worker.ProblemID)
+		outputDir = filepath.Join(worker.WorkDir)
+	)
+	sampleId := strconv.FormatInt(int64(sampleID), 10)
+	input := strings.Join([]string{sampleId, "in"}, ".")
+	output := strings.Join([]string{sampleId, "out"}, ".")
+	erroR := strings.Join([]string{sampleId, "err"}, ".")
+
+	inputFileName := filepath.Join(sampleDir, input)
+	outputFileName := filepath.Join(outputDir, output)
+	errorFileName := filepath.Join(outputDir, erroR)
+
 	files, err := prepareFiles(inputFileName, outputFileName, errorFileName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to prepare files: %v", err)
@@ -144,8 +201,8 @@ func (worker *Worker) load(sampleID int, lang lang.Lang) (*sandbox.Runner, error
 		Args: lang.CompileArgs(),
 		Env:  os.Environ(),
 		// ExecFile: execFile,
-		Files:       fds,
-		WorkDir:     worker.WorkDir,
+		Files: fds,
+		// WorkDir:     worker.WorkDir,
 		Seccomp:     filter,
 		RLimits:     rlimits.PrepareRLimit(),
 		Limit:       limit,
