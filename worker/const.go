49d3e8058c975ad95728f1f5e411d417693a7f97
isther <28251536@qq.com>
2022-04-16 21:43:36 +0800

    Modifed run dir

--- worker/const.go
+++ worker/const.go
@@ -3,7 +3,7 @@ package worker
 import "path/filepath"
 
 var (
-	runDir        = filepath.Join(".", "tmp")
+	runDir        = filepath.Join("/", "tmp")
 	sampleBaseDir = filepath.Join(".", "sample")
 	pathEnv       = "PATH=/usr/local/bin:/usr/bin:/bin"
 )
