c56ab73031c34357ed87d1783866b0642a8fba70
isther <28251536@qq.com>
2022-04-18 15:14:19 +0800

    Added worker

--- worker/worker.go
+++ worker/worker.go
@@ -1,114 +1,83 @@
 package worker
 
 import (
-	"context"
 	"net/http"
-	"os"
-	"os/signal"
-	"path/filepath"
-	"time"
 
 	"github.com/SXUOJ/judge/lang"
-	"github.com/SXUOJ/judge/runner"
-	"github.com/SXUOJ/judge/sandbox"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
 type Worker struct {
-	SubmitID string
-
-	FileName string
-	Type     string
-	WorkDir  string
-
-	AllowProc     bool
-	TimeLimit     uint64
-	RealTimeLimit uint64
-	MemoryLimit   uint64
-	OutputLimit   uint64
-	StackLimit    uint64
-
-	compiler Compiler
-	runner   Runner
+	C    *gin.Context
+	Done chan bool
+	Lang lang.Lang
+	Compiler
+	Runner
 }
 
-func (worker *Worker) Run(c *gin.Context) {
-	var (
-		sourcePath = filepath.Join(worker.WorkDir, worker.FileName)
-		binaryPath = filepath.Join(worker.WorkDir, worker.SubmitID)
-	)
+type Compiler interface {
+	Start(chan interface{})
+}
+type Runner interface {
+	Start(chan interface{}, chan bool)
+}
 
-	lang, err := lang.NewLang(worker.Type, sourcePath, binaryPath)
-	if err != nil {
-		msg := "New lang failed"
-		errReturn(c, msg)
-		return
+func (w *Worker) Run() {
+	if w.Lang.NeedCompile() {
+		resChan := make(chan interface{})
+		go w.Compiler.Start(resChan)
+
+		compiling := true
+		for compiling {
+			select {
+			case res := <-resChan:
+				if res != nil {
+					msg := "Compile Error"
+					logrus.Error(msg)
+					w.C.JSON(http.StatusOK, gin.H{
+						"msg":    msg,
+						"result": res,
+					})
+					w.stop()
+					return
+				} else {
+					logrus.Info("Compile success")
+					compiling = false
+				}
+			}
+		}
+
 	}
 
-	if lang.NeedCompile() {
-		compiler, err := NewCompiler(worker, lang, c)
-		if err != nil {
-			msg := "New compiler failed"
-			errReturn(c, msg)
+	done := make(chan bool)
+	resChan := make(chan interface{})
+	go w.Runner.Start(resChan, done)
+
+	running := true
+	for running {
+		select {
+		case <-done:
+			running = false
+		case res := <-resChan:
+			w.C.JSON(http.StatusOK, gin.H{
+				"msg":    "ok",
+				"result": res,
+			})
+		}
+	}
+	w.stop()
+}
+
+func (w *Worker) stop() {
+	w.Done <- true
+}
+
+func (w *Worker) Wait() {
+	for {
+		select {
+		case <-w.Done:
 			return
 		}
-		if err := compiler.Run(); err != nil {
-			logrus.Error(err.Error())
-			return
-		}
 	}
-
-	run, err := NewRunner(worker, lang, c)
-	if err != nil {
-		msg := "New runner failed"
-		errReturn(c, msg)
-		return
-	}
-	run.Run()
-	return
-}
-
-func run(r *sandbox.Runner, realTimeLimit uint64) (*runner.Result, error) {
-	var (
-		rt runner.Result
-	)
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
-
-	// Run tracer
-	sTime := time.Now()
-	c, cancel := context.WithTimeout(context.Background(), time.Duration(int64(realTimeLimit)*int64(time.Second)))
-	defer cancel()
-
-	s := make(chan runner.Result, 1)
-	go func() {
-		s <- r.Run(c)
-	}()
-	rTime := time.Now()
-
-	select {
-	case <-sig:
-		cancel()
-		rt = <-s
-		rt.Status = runner.StatusSystemError
-
-	case rt = <-s:
-	}
-	eTime := time.Now()
-
-	if rt.SetUpTime == 0 {
-		rt.SetUpTime = rTime.Sub(sTime)
-		rt.RunningTime = eTime.Sub(rTime)
-	}
-
-	return &rt, nil
-}
-
-func errReturn(c *gin.Context, msg string) {
-	logrus.Error(msg)
-	c.JSON(http.StatusOK, gin.H{
-		"msg": msg,
-	})
 }
